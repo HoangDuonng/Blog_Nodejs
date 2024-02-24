770236c438bbfd181a0ca649846b8e03300c76e3
HoangDuonng <Nguuyen1801@gmail.com>
2024-02-24 23:41:29 +0700

    Boostrap 4

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const handlebars = require('express-handlebars');
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+//HTPP logger
+app.use(morgan('combined'));
+
+//Template engine
+app.engine('hbs', handlebars.engine({
+  extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources', 'views'));
+
+app.get('/', (req, res) => {
+  res.render('home');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
